3fab1b447ae91cf46efc4940a4fb9b5b1f2c606b
makerbro <makerbro@acrobotic.com>
2016-05-15 21:19:22 +0000

    implementing frequency changes

--- PCA9685.py
+++ PCA9685.py
@@ -5,17 +5,21 @@ import math
 import smbus
 
 # ===========================================================================
-# Example Python Servo Code for Hobbytronics PWM/Servo board
-# http://www.hobbytronics.co.uk/pwm-servo
-#
-# Set servo position using format similar to Arduino where position
-# is defined in degrees 0 to 180 with 90 being the center position
-# 
-# Pulse type is defined as 'standard' (0) generates pulse of 1ms to 2ms
-#                          'extended' (1) generates pulse of 0.6ms to 2.4ms
+# PCA9685 Python Library
+# Based on example Python Servo Code for Hobbytronics PWM/Servo board
+# Author: MakerBro for ACROBOTIC Industries
+# Date: 05/08/2016
 # ===========================================================================
 
 bus = smbus.SMBus(1)  # RPi V2 - for V1 RPi use SMBus(0)
+I2C_ADDR  = 0x60
+# RST | EXTCLK | AUTOINC | SLEEP | SUB1 | SUB2 | SUB3 | ALL
+REG_MODE1       = 0x00  
+REG_PRESC       = 0xFE
+REG_LED0_ON_L   = 0x06
+REG_LED0_ON_H   = 0x07
+REG_LED0_OFF_L  = 0x08
+REG_LED0_OFF_H  = 0x09
 
 # Define a class for our servo functions
 class Driver:
@@ -27,45 +31,53 @@ class Driver:
     def setLowPowerMode(self, enable=True):
         if enable:
             # Low power mode. Oscillator off.
-            bus.write_byte_data(self.address, 0x00, 0x10)
+            bus.write_byte_data(self.address, REG_MODE1, 0x10)
         else:
-            pass # need to read the register
+            # TODO: need to read the register
+            bus.write_byte_data(self.address, REG_MODE1, 0x00)
 
     def setExtClock(self, enable=True):
         if enable:
-            setLowPowerMode(True)
+            self.setLowPowerMode(True)
             #  Write logic 1s to both the SLEEP and EXTCLK bits in MODE1.
-            bus.write_byte_data(self.address, 0x00, 0x50)
+            bus.write_byte_data(self.address, REG_MODE1, 0x50)
         else:
             pass # sticky bit so needs reset
 
     def setFreq(self, frequency):
         self.frequency = frequency
         self.duration_1ms = ((4096*frequency)/1000);  # This is 1ms duration
-        prescale = 16000000.0
+        prescale = 25000000.0   #25MHz Oscillator Clock
         prescale /= 4096.0
         prescale /= float(frequency)
         prescale -= 1.0
         prescale8 = int(math.floor(prescale + 0.5))
-        bus.write_byte_data(self.address, 0xFE, prescale8) # set the prescaler
+        self.setLowPowerMode(True)
+        #self.setExtClock(True)
+        bus.write_byte_data(self.address, REG_PRESC, prescale8) # set the prescaler
+        self.setLowPowerMode(False)
         time.sleep(0.01)
-        bus.write_byte_data(self.address, 0x00, 0xA0)      # Set Auto-Increment on, enable restart
+        bus.write_byte_data(self.address, REG_MODE1, 0xA0)      # Set Auto-Increment on, enable restart
 
-    def setPWM(self, channel, duration):
-        bus.write_byte_data(self.address, 0x06+(4*channel), 0x00)
-        bus.write_byte_data(self.address, 0x07+(4*channel), 0x00)
-        bus.write_byte_data(self.address, 0x08+(4*channel), duration & 0xFF)
-        bus.write_byte_data(self.address, 0x09+(4*channel), duration >> 8) 
+    def setPWM(self, channel, off_count, on_count=0x00):
+        bus.write_byte_data(self.address, REG_LED0_ON_L+(4*channel), on_count & 0xFF)
+        bus.write_byte_data(self.address, REG_LED0_ON_H+(4*channel), on_count >> 8)
+        bus.write_byte_data(self.address, REG_LED0_OFF_L+(4*channel), off_count & 0xFF)
+        bus.write_byte_data(self.address, REG_LED0_OFF_H+(4*channel), off_count >> 8) 
 
-# Create an instance of PWMServo class
-driver = Driver(0x60)
-driver.setFreq(20000)
+    def setOn(self, channel):
+        bus.write_byte_data(self.address, REG_LED0_ON_H+(4*channel), 0x10)
 
-driver.setPWM(1, 4095)
-i=0
-while (True):
-    if i > 4096:
-        i = 0
-    driver.setPWM(0, i)
-    time.sleep(0.01)
-    i+=1
+    def setOff(self, channel):
+        bus.write_byte_data(self.address, REG_LED0_OFF_H+(4*channel), 0x10)
+
+# Create an instance of PWM Driver class
+driver = Driver(I2C_ADDR)
+driver.setFreq(1000)
+
+pwm_enable = 1
+pwm_direction = 0
+
+#driver.setPWM(pwm_direction, 4095)
+driver.setOn(pwm_direction)
+driver.setPWM(pwm_enable, 512)
